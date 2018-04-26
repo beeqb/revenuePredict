e920d434adbb11121c508a8fce0a87c111da15cf
vivian <821956256@qq.com>
2018-04-26 19:24:34 -0400

    x

--- amazon crawler/link.py
+++ amazon crawler/link.py
@@ -1,22 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
 
-file = open(r"parseddata.txt", "w")
+file = open(r"underarmourLinks.txt", "w")
 for i in range(1,401):
-    print("getting links from page{}...".format(i))
-    url = "https://www.amazon.com/s/ref=sr_pg_"+str(i)+"?sf=qz%2Crba&fst=as%3Aoff&rh=n%3A3375251%2Ck%3Anike%2Cp_89%3ANIKE&page="+str(i)+"&keywords=nike&unfiltered=1&ie=UTF8"
-
-    # add header
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'
-    }
-    r = requests.get(url, headers=headers)
-    soup = BeautifulSoup(r.content, "lxml")
-
-    links = soup.find_all('a', {'class': 'a-size-small a-link-normal a-text-normal'})
-
-    for link in links:
+	print("getting links from page{}...".format(i))
+	
+	# url = "https://www.amazon.com/s/ref=sr_pg_"+str(i)+"?sf=qz%2Crba&fst=as%3Aoff&rh=n%3A3375251%2Ck%3Anike%2Cp_89%3ANIKE&page="+str(i)+"&keywords=nike&unfiltered=1&ie=UTF8"
+	# url = "https://www.amazon.com/s/ref=sr_pg_"+str(i)+"?fst=as%3Aoff&rh=n%3A3375251%2Ck%3Aadidas%2Cp_89%3Aadidas%7Cadidas+Originals&page="+str(i)+"&keywords=adidas&unfiltered=1&ie=UTF8"
+	url = "https://www.amazon.com/s/ref=sr_pg_"+str(i)+"?fst=as%3Aoff&rh=i%3Aaps%2Ck%3Aunder+armour%2Cp_89%3AUnder+Armour&page="+str(i)+"&keywords=under+armour&ie=UTF8&qid=1524681688";
+	# add header
+	headers = {
+		'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'
+		}
+	r = requests.get(url, headers=headers)
+	soup = BeautifulSoup(r.content, "lxml")
+	
+	links = soup.find_all('a', {'class': 'a-size-small a-link-normal a-text-normal'})
+	
+	for link in links:
 		if link.get('href').find("customerReviews")>0:
 			file.write(link.get('href')+ '\n')
-		
+			
 file.close()
--- amazon crawler/review.py
+++ amazon crawler/review.py
@@ -8,6 +8,8 @@ from bs4 import BeautifulSoup
 import time
 import random
 import pymysql.cursors
+import requests
+
 
 
 def getCount(name,url):
@@ -19,7 +21,7 @@ def getCount(name,url):
                                   charset = 'utf8mb4')
     try:
         with connection.cursor() as cursor:
-            sql = "select * from nike_products where name=(%s)"
+            sql = "select * from `"+filename+"_products` where name=(%s)"
 
             cursor.execute(sql,(name))
             results = cursor.fetchall()
@@ -30,24 +32,25 @@ def getCount(name,url):
         connection.close()
     #get basic information about the product
     while 1:
-        html1 = urllib.urlopen(url).read()
-        html1 = str(html1)
-        soup1 = BeautifulSoup(html1,'lxml')
-        productCount = soup1.find_all(attrs={"data-hook":"total-review-count"})
-        productAverageRating = soup1.find_all(attrs={"data-hook":"average-star-rating"})
-        productPrice = soup1.find_all('span',{'class': 'a-color-price arp-price'})
-
-        try:
-            if len(productCount[0].string)>3:
-                numbers = productCount[0].string.split(',')
-                count = int(numbers[0])*1000+int(numbers[1])
-            else:
-                count = int(productCount[0].string)
-            averageRating = productAverageRating[0].string
-           
-            break
-        except:
-            continue
+        #html1 = urllib.urlopen(url).read()
+        #html1 = str(html1)
+		r = requests.get(url, headers=headers)
+        #soup1 = BeautifulSoup(html1,'lxml')
+		soup1 = BeautifulSoup(r.content,'lxml')
+		productCount = soup1.find_all(attrs={"data-hook":"total-review-count"})
+		productAverageRating = soup1.find_all(attrs={"data-hook":"average-star-rating"})
+		productPrice = soup1.find_all('span',{'class': 'a-color-price arp-price'})
+		
+		try:
+			if len(productCount[0].string)>3:
+				numbers = productCount[0].string.split(',')
+				count = int(numbers[0])*1000+int(numbers[1])
+			else:
+				count = int(productCount[0].string)
+			averageRating = productAverageRating[0].string
+			break
+		except:
+			continue
     if len(productPrice):
         price = productPrice[0].string
     else:
@@ -59,7 +62,7 @@ def getCount(name,url):
                                   charset = 'utf8mb4')
     try:
         with connection.cursor() as cursor:
-            sql = "insert into `nike_products` (name,price,totalReviewCount,averageRating) values (%s,%s,%s,%s)"
+            sql = "insert into `"+filename+"_products` (name,price,totalReviewCount,averageRating) values (%s,%s,%s,%s)"
 
             cursor.execute(sql,(name,price,count,averageRating))
 
@@ -71,58 +74,63 @@ def getCount(name,url):
     return count
     
 def crawler(name,url):
-    tryCrawl = 1
-    while 1:
-        print("{} try".format(tryCrawl))
-        tryCrawl+=1
-        html1 = urllib.urlopen(url).read()
-        html1 = str(html1)
+	tryCrawl = 1
+	while 1:
+		if tryCrawl>500:
+			return -1
+		print("{} try".format(tryCrawl))
+		tryCrawl+=1
+		#html1 = urllib.urlopen(url).read()
+		#html1 = str(html1)
+		r = requests.get(url, headers=headers)
+		#soup1 = BeautifulSoup(html1,'lxml')
+		soup1 = BeautifulSoup(r.content,'lxml')
+		reviewAuthor = soup1.find_all(attrs={"data-hook":"review-author"})
+		reviewDate = soup1.find_all(attrs={"data-hook":"review-date"})
+		reviewText = soup1.find_all(attrs={"data-hook":"review-body"})
+		reviewRating = soup1.find_all(attrs={"data-hook":"review-star-rating"})
+		reviewTitle = soup1.find_all(attrs={"data-hook":"review-title"})
+		try:
+			test = reviewText[0].string
+			break
+		except:
+			continue
 
-        soup1 = BeautifulSoup(html1,'lxml')
-        reviewAuthor = soup1.find_all(attrs={"data-hook":"review-author"})
-        reviewDate = soup1.find_all(attrs={"data-hook":"review-date"})
-        reviewText = soup1.find_all(attrs={"data-hook":"review-body"})
-        reviewRating = soup1.find_all(attrs={"data-hook":"review-star-rating"})
-        reviewTitle = soup1.find_all(attrs={"data-hook":"review-title"})
-        try:
-            test = reviewText[0].string
-            break
-        except:
-            continue
+	index = 0
+	for cTime in reviewDate:
+		commentTime = cTime.string
+		text = reviewText[index].string
+		rating = reviewRating[index].string
+		title = reviewTitle[index].string
+		author = reviewAuthor[index].string
+		index=index+1
 
-    index = 0
-    for cTime in reviewDate:
-        commentTime = cTime.string
-        text = reviewText[index].string
-        rating = reviewRating[index].string
-        title = reviewTitle[index].string
-        author = reviewAuthor[index].string
-        index=index+1
-
-
-        connection  = pymysql.connect(host = 'localhost',
+		
+		connection  = pymysql.connect(host = 'localhost',
                                   user = 'root',
                                   password = '123456',
                                   db = 'amazon_reviews',
                                   charset = 'utf8mb4')
-        try:
-            with connection.cursor() as cursor:
-                sql = "insert into `nike_reviews` (comment,date,rating,title,author,productName) values (%s,%s,%s,%s,%s,%s)"
-
-                cursor.execute(sql,(text,commentTime,rating,title,author,name))
-
-
-                connection.commit()
-        except:
-            continue
-        finally:
-            connection.close()
-            
-    return len(reviewText)
+		try:
+			with connection.cursor() as cursor:
+				sql = "insert into `"+filename+"_reviews` (comment,date,rating,title,author,productName) values (%s,%s,%s,%s,%s,%s)"
+				cursor.execute(sql,(text,commentTime,rating,title,author,name))
+				connection.commit()
+		except:
+			continue
+		finally:
+			connection.close()
+	return len(reviewText)
 
 
 links = []
-file = open("parseddata.txt") 
+#filename = "nike"
+filename = "adidas"
+#filename = "underarmour"
+file = open(filename+"Links.txt") 
+headers = {
+		'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'
+		}
 while 1:
     line = file.readline()
     if not line:
@@ -144,6 +152,8 @@ for i in range(1,len(links)):
         print("same product exists")
     else:
         page = count/10 + 2
+        if count == 0:
+            page = 0
         for j in range(1,page):
             print("downloading reviews from page{}...".format(j))
             productUrl =url + "&pageNumber="+str(j)
